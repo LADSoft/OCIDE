9a412c6742b7c907d7fb308ab762578d30d5c6e7
David Lindauer <touchstone222@runbox.com>
2020-01-29 22:32:13 -0500

    fix build

--- appveyorversion.bat
+++ appveyorversion.bat
@@ -1,9 +1,9 @@
 set COMMAVERSION=%APPVEYOR_BUILD_VERSION:.=,%
-echo #define STRING_VERSION "%APPVEYOR_BUILD_VERSION%" > c:\orangec\src\version.h
-echo #define PRODUCT_VERSION %COMMAVERSION% >> c:\orangec\src\version.h
-echo #define CLIB_VERSION %COMMAVERSION% >> c:\orangec\src\version.h
-echo #define PRODUCT_STRING_VERSION STRING_VERSION >> c:\orangec\src\version.h
-echo #define PRODUCT_NAME "Orange C Compiler Package" >> c:\orangec\src\version.h
-echo #define CLIB_STRING_VERSION STRING_VERSION >> c:\orangec\src\version.h
-echo #define COPYRIGHT "Copyright (C) LADSoft 2006-2019" >> c:\orangec\src\version.h
-echo #define LIBRARY_COPYRIGHT "Copyright (C) LADSoft 1994-2019" >> c:\orangec\src\version.h
+echo #define STRING_VERSION "%APPVEYOR_BUILD_VERSION%" > c:\ocide\src\version.h
+echo #define PRODUCT_VERSION %COMMAVERSION% >> c:\ocide\src\version.h
+echo #define CLIB_VERSION %COMMAVERSION% >> c:\ocide\src\version.h
+echo #define PRODUCT_STRING_VERSION STRING_VERSION >> c:\ocide\src\version.h
+echo #define PRODUCT_NAME "Orange C Compiler Package" >> c:\ocide\src\version.h
+echo #define CLIB_STRING_VERSION STRING_VERSION >> c:\ocide\src\version.h
+echo #define COPYRIGHT "Copyright (C) LADSoft 2006-2019" >> c:\ocide\src\version.h
+echo #define LIBRARY_COPYRIGHT "Copyright (C) LADSoft 1994-2019" >> c:\ocide\src\version.h
